9600367707c86d90acc7fe26d1583dedbb7e6e46
ilsu-sunal <sunalilsu@gmail.com>
2024-03-27 16:50:05 +0300

    problemler çözüldü

--- src/main/java/org/example/company/Car.java
+++ src/main/java/org/example/company/Car.java
@@ -26,15 +26,15 @@ public class Car {
 
     public String startEngine(){
         System.out.println(getClass().getSimpleName());
-       return "the car's engine is starting ";
+       return "the car's engine is starting";
     }
     public String accelerate(){
         System.out.println(getClass().getSimpleName());
-        return "the car is accelerating ";
+        return "the car is accelerating";
     }
     public String brake(){
         System.out.println(getClass().getSimpleName());
-        return "the car is braking ";
+        return "the car is braking";
     }
     @Override
     public String toString() {
